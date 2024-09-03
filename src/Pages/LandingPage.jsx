db15aa0f92c4d8ab2acffb6b011cf186569329ea
VISHNU M K <mail.vishnumk@gmail.com>
2024-09-03 10:42:23 +0530

    initial commit

---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+import NavBar from '../Components/NavBar'
+import Hero from '../Components/Hero'
+
+const LandingPage = () => {
+    
+    return (
+        <Fragment>
+            <NavBar />
+            <Hero />
+        </Fragment>
+    )
+}
+
+export default LandingPage
