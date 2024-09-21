bef954281cae0d20c291e6c199eae1d0b245930f
VISHNU M K <mail.vishnumk@gmail.com>
2024-09-21 10:54:13 +0530

    todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { Fragment } from "react"
 import LandingPage from "./Pages/LandingPage"
+import Todo from "./Pages/Todo"
+import { Toaster } from "react-hot-toast"
+
 
 const App = () => {
-    return <LandingPage />
+    return <Fragment>
+        {/* <LandingPage /> */}
+        <Todo />
+        <Toaster position="top-right"/>
+    </Fragment>
 }
 
 export default App
